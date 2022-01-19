30419f4742dd2ee7e76625e3cf40dc8089227d12
IndraSatya94 <minsatya94@gmail.com>
2022-01-19 15:08:35 +0800

    initial commit

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace Database\Seeders;
+
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->where('email', 'admin@gmail.com')->delete();
+
+        DB::table('users')->insert([
+            'name' => 'John Doe',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('123456'),
+            'type' => 'admin',
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'John user',
+            'email' => 'user@gmail.com',
+            'password' => bcrypt('123456'),
+            'type' => 'user',
+        ]);
+    }
+}
